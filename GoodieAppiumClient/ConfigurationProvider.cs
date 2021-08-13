f8dcb929b87ff40b303be1a4b74bd5af38a867ad
Lukasz Mroz <mrozikpl@gmail.com>
2021-08-13 15:36:33 +0200

    feature: fullfilled ConfiugrationProvider

---
+++ GoodieAppiumClient/ConfigurationProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+
+namespace AppiumClient
+{
+  public class ConfigurationProvider
+  {
+    private readonly IConfiguration _configuration;
+
+    public ConfigurationProvider()
+    {
+      _configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false, true)
+        .Build();
+    }
+  }
+}
